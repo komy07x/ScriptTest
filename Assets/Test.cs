3b94471a485d3b7881dfe903fb56bc364b5aa347
小針茉優子 <komy8115@ezweb.ne.jp>
2017-03-08 01:58:06 +0900

    ScriptTestプロジェクト　C#課題
    配列を宣言して出力。int型の配列arrayを初期化、配列の各要素の値を順番に表示、for文で逆順に表示した。

---
+++ Assets/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		int[] array = { 1, 2, 3, 4, 5 };
+
+		for (int i = 0; i < array.Length; i++) {
+			Debug.Log (array [i]);
+		}
+
+		for (int i = array.Length - 1; 0 <= i; i--) {
+			Debug.Log (array [i]);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
